98651503befcfce0616733b05bf5678e02b0ca14
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-03-22 15:45:48 -0500

    vote data included in single user info

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -5,7 +5,7 @@ const { Post, User, Vote } = require('../../models');
 router.get("/", (req, res) => {
     console.log("======================");
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -26,7 +26,7 @@ router.get('/:id', (req, res) => {
       where: {
         id: req.params.id
       },
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
       include: [
         {
           model: User,
