42af1f15a79a9a8695fda741c862d5db2440f82f
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-03-23 16:24:10 -0500

    created logout function

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require("path");
@@ -7,10 +9,20 @@ const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
