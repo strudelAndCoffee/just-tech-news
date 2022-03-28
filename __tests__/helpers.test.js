b68d52824f2256e6b5f07a946d17475b5bc2df39
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-03-27 19:55:21 -0500

    tests pass for format date and plural helpers

---
+++ __tests__/helpers.test.js
@@ -0,0 +1,12 @@
+const { format_date, format_plural } = require('../utils/helpers');
+
+test('format_date() returns a date string', () => {
+    const date = new Date('2020-03-20 16:12:03');
+  
+    expect(format_date(date)).toBe('3/20/2020');
+});
+
+test('format_plural() returns a properly pluralized word', () => {
+    expect(format_plural("Lion", 1)).toBe("Lion");
+    expect(format_plural("Tiger", 2)).toBe("Tigers");
+});
